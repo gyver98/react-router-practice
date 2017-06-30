9d87affdb3ad2b77e87368ed6292b26d85fdd729
Matthew Choi <matthewchoi7@gmail.com>
2017-06-30 10:55:55 +1000

    asynchronous import component: deleting SplitMe

--- src/lib/asyncRoute.js
+++ src/lib/asyncRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function asyncComponent(getComponents) {
+export default function asyncComponent(getComponent) {
   return class AsyncComponent extends React.Component {
     static Component = null;
     state = { Component: AsyncComponent.Component };
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Home, About, Posts } from 'pages';
+import { Home, About, Posts } from 'pages/index.async.js';
 import Menu from 'components/Menu';
 
 class App extends Component {
-
-  state = {
-    SplitMe: null
-  }
-
-  showSplitMe = () => {
-    import('components/SplitMe').then(({default: Component}) => {
-      this.setState({
-        SplitMe: Component
-      });
-    });
-  }
-
+ 
   render() {
-    const { SplitMe } = this.state;
-
     return (
       <div>
         <Menu/>
-        { SplitMe && <SplitMe/>}
-        <button onClick={this.showSplitMe}>ClickMe</button>
         <Route exact path="/" component={Home}/>
         <Switch>
           <Route path="/about/:name" component={About}/>
